a1d11b0ab1ed47603095cc9f4ad6285e0f93c350
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-04-03 11:16:02 +0700

    membuat perulangan array untuk input dan output yang dibuat

--- latihan2/latihan2.cpp
+++ latihan2/latihan2.cpp
@@ -13,3 +13,21 @@ struct mahasiswa {
 	AlamatDetail alamat;
 	int umur;
 };
+
+int main() {
+	mahasiswa mhs[3];
+
+	for (int i = 0; i < 3; i++) {
+		cout << " Masukkan NIM = ";
+		cin.getline(mhs[i].NIM, 12);
+		cout << " Masukkan Nama = ";
+		cin.getline(mhs[i].nama, 50);
+		cout << " Alamat = ";
+		cout << "\n\tMasukkan Desa = ";
+		cin.getline(mhs[i].alamat.desa, 25);
+		cout << "\tMasukkan Kota = ";
+		cin.getline(mhs[i].alamat.kota, 25);
+		cout << " Masukkan Umur = ";
+		cin >> mhs[i].umur;
+	}
+}
