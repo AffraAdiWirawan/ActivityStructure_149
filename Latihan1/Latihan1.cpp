eac0b21f455273c0459f4075b1914ed4791a1941
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-04-03 10:53:09 +0700

    Mmebuat input dan output

--- Latihan1/Latihan1.cpp
+++ Latihan1/Latihan1.cpp
@@ -13,3 +13,19 @@ struct mahasiswa {
 	AlamatDetail alamat;
 	int umur;
 };
+
+int main() {
+	mahasiswa mhs;
+
+	cout << " Masukkan NIM = ";
+	cin >> mhs.NIM;
+	cout << " Masukkan Nama = ";
+	cin >> mhs.nama;
+	cout << " Masukkan Alamat : ";
+	cout << "\n\tMasukkan Desa = ";
+	cin >> mhs.alamat.desa;
+	cout << "\tMasukkan Kota = ";
+	cin >> mhs.alamat.kota;
+	cout << " Masukkan Umur = ";
+	cin >> mhs.umur;
+}
