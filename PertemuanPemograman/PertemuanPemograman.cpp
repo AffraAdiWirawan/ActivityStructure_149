3ee1377950051fbff6481a182e66dfac97c4a72c
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-04-03 10:25:21 +0700

    membuat prosedur input dengan cout

--- PertemuanPemograman/PertemuanPemograman.cpp
+++ PertemuanPemograman/PertemuanPemograman.cpp
@@ -7,4 +7,22 @@ struct mahasiswa {
 	string nama;
 	string alamat;
 	int umur;
+};
+
+int main() {
+	mahasiswa mhs1, mhs2;
+
+	mhs1.NIM = "20220140149";
+	mhs1.nama = "Affra Adi";
+	mhs1.alamat = " Bekasi ";
+	mhs1.umur = 20;
+
+	cout << " Masukkan NIM : ";
+	cin >> mhs2.NIM;
+	cout << " Masukkan Nama : ";
+	cin >> mhs2.nama;
+	cout << " Masukkan Alamat : ";
+	cin >> mhs2.alamat;
+	cout << "Masukkan Umur : ";
+	cin >> mhs2.umur;
 }
